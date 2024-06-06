98358fad5ae1760b9c28c7b18694a9a07268c393
EFEJOTTA <162126572+EFEJOTTA@users.noreply.github.com>
2024-06-05 23:52:01 -0300

    Add files via upload
    Adição da função make guess à classe jogo da forca

--- game.py
+++ game.py
@@ -11,4 +11,17 @@ class Jogo_da_forca:
         display = ''.join([letter if letter in self.guesses else '_' for letter in self.word])
         print(f"Palavra: {display}")
 
+    def make_guess(self, guess):
+        if guess in self.guesses:
+            print("Você já adivinhou essa letra.")
+            return
+        
+        self.guesses.append(guess)
+        
+        if guess not in self.word:
+            self.attempts_left -= 1
+            print(f"Errado! Tentativas restantes: {self.attempts_left}")
+        else:
+            print("Correto!")
+
 
