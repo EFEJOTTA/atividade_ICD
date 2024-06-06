96d2c0c760222a9cd744f9f223280630cfcf2bc2
EFEJOTTA <162126572+EFEJOTTA@users.noreply.github.com>
2024-06-05 23:50:12 -0300

    Add files via upload
    Adição da função display word à classe jogo da força

--- game.py
+++ game.py
@@ -7,3 +7,8 @@ class Jogo_da_forca:
         self.max_attempts = 6
         self.attempts_left = self.max_attempts
 
+    def display_word(self):
+        display = ''.join([letter if letter in self.guesses else '_' for letter in self.word])
+        print(f"Palavra: {display}")
+
+
